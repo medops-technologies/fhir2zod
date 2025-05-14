92e93a8093d2be595ce6ec1a72fcde41644391c2
dermasugita <itsukiisogin@gmail.com>
2025-05-14 10:22:59 +0900

    added: primitive-type url->name map

--- src/types/primitiveTypeSchemaCodes.ts
+++ src/types/primitiveTypeSchemaCodes.ts
@@ -39,6 +39,58 @@ export const initializePrimitiveTypeSchemasCodes = (): PrimitiveTypeCodeMap => {
     // Other types
     primitiveTypes.set('boolean', 'z.boolean()')
     primitiveTypes.set('base64Binary', 'z.string()')
+    primitiveTypes.set('xhtml', 'z.string()')
 
     return primitiveTypes
+}
+
+export const getPrimitiveTypeFromUri = (uri: string): string => {
+    // FHIRPath System types
+    if (uri === 'http://hl7.org/fhirpath/System.String') return 'string'
+    if (uri === 'http://hl7.org/fhirpath/System.Boolean') return 'boolean'
+    if (uri === 'http://hl7.org/fhirpath/System.Integer') return 'integer'
+    if (uri === 'http://hl7.org/fhirpath/System.Decimal') return 'decimal'
+    if (uri === 'http://hl7.org/fhirpath/System.Date') return 'date'
+    if (uri === 'http://hl7.org/fhirpath/System.DateTime') return 'dateTime'
+    if (uri === 'http://hl7.org/fhirpath/System.Time') return 'time'
+
+    // Official FHIR StructureDefinition URIs
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/base64Binary')
+        return 'base64Binary'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/boolean')
+        return 'boolean'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/canonical')
+        return 'canonical'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/code') return 'code'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/date') return 'date'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/dateTime')
+        return 'dateTime'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/decimal')
+        return 'decimal'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/id') return 'id'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/instant')
+        return 'instant'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/integer')
+        return 'integer'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/markdown')
+        return 'markdown'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/oid') return 'oid'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/positiveInt')
+        return 'positiveInt'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/string')
+        return 'string'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/time') return 'time'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/unsignedInt')
+        return 'unsignedInt'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/uri') return 'uri'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/url') return 'url'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/uuid') return 'uuid'
+    if (uri === 'http://hl7.org/fhir/StructureDefinition/xhtml') return 'xhtml'
+
+    // Fallback: extract the type name from the URI
+    const parts = uri.split('/')
+    const lastPart = parts[parts.length - 1]
+    const cleanType = lastPart.split('#').pop() || lastPart
+
+    return cleanType.toLowerCase()
 }
